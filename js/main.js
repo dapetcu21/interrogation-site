5f111320f4974ecc887c9e5e1bae29f05626848b
Marius Petcu <dapetcu21@gmail.com>
2016-10-02 21:45:26 +0300

    Flipping nav menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
 (function () {
+  var elem = window.document.body;
+
+  var scrolledState = false;
+  function onScroll() {
+    var newState = window.scrollY > 30;
+    if (newState === scrolledState) { return; }
+    scrolledState = newState;
+    if (scrolledState) {
+      elem.className += ' scrolled';
+    } else {
+      elem.className = elem.className.replace(/(?:^|\s)scrolled(?!\S)/g , '');
+    }
+  }
+  onScroll();
+
+  //Debounce the scroll event
+
   var nextAllowedScroll = 0;
+  var scheduledFire = 0;
+  function timerFired() {
+    scheduledFire = 0;
+    onScroll();
+  }
+
   window.onscroll = function () {
     var time = Date.now();
-    if (time < nextAllowedScroll) { return; }
+    if (time < nextAllowedScroll) {
+      if (!scheduledFire) {
+        scheduledFire = setInterval(timerFired, 200);
+      }
+      return;
+    }
     nextAllowedScroll = time + 200;
-    console.log(document.scrollTop);
+    onScroll();
   }
-})()
+})();
